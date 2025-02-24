0eb16ede0d199ad0c735c0be52f214ccf73b8780
maxim-f1 <maxconal228@gmail.com>
2025-02-24 18:58:54 +0300

    [PROJECT-141] 1. В регулярку добавлен пробел.

--- web/src/api/auth/dto.py
+++ web/src/api/auth/dto.py
@@ -8,7 +8,7 @@ from src.domain.abc.dto import ABCDTO, ABCResponse
 class SignUpDTO(ABCDTO):
     email: EmailStr = Field(..., max_length=60, description='Почта пользователя')
     username: str = Field(
-        ..., max_length=20, pattern=re.compile(r'^[a-zA-Z0-9а-яА-ЯёЁ_-]+$'),
+        ..., max_length=20, pattern=re.compile(r'^[a-zA-Z0-9а-яА-ЯёЁ_\s-]+$'),
         description='Псевдоним пользователя'
     )
     password: str = Field(
