6e345a5029b50d7c91053dcc31094477dbdc7ccb
maxim-f1 <maxconal228@gmail.com>
2025-03-01 15:41:05 +0300

    [PROJECT-120] Обновление информации о книге и ее жанрах 1. PUT /v1/books 2. Добавлена функция по удаление жанров книг.

--- web/src/domain/book_genre/dal.py
+++ web/src/domain/book_genre/dal.py
@@ -1,6 +1,16 @@
+from uuid import UUID
+
+from sqlalchemy import delete
+
 from src.domain.abc.dal import ABCDAL
 from src.domain.book_genre.model import BookGenre
 
 
 class BookGenreDAL(ABCDAL[BookGenre]):
-    pass
+    async def delete_by_book(self, book_id: UUID):
+        query = delete(
+            self.model
+        ).where(
+            self.model.book_id == book_id
+        )
+        await self.session.execute(query)
